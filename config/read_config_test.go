13d2a7cd4c7624e0a7bb9a23d22f133d69179944
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2021-07-11 20:17:17 +0200

    Set CI

--- config/read_config_test.go
+++ config/read_config_test.go
@@ -1,1 +1,17 @@
 package config
+
+import (
+	"testing"
+)
+
+func TestProcessNoConfigFilePresent(t *testing.T) {
+
+	_, err := ReadConfig()
+	if err == nil {
+		t.Errorf("ReadConfig method without any valid config file should fail.")
+	} else {
+		if err.Error() != "Fatal error reading config file: Config File \"config\" Not Found in \"[/etc/music-manager]\"" {
+			t.Errorf("Default config should be in /etc/music-manager/config.toml, not in other place, error was '%s'.", err.Error())
+		}
+	}
+}
